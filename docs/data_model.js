6fcde89c38e79d2196bef2137d176a61a99fe4d4
Nick Escallon <nickescallon@gmail.com>
2014-11-16 18:26:21 -0500

    added sample json

--- docs/data_model.js
+++ docs/data_model.js
@@ -1,61 +1,63 @@
 {
-  domain: '247sports.com',
-  name: '247sports',
-  suffix: '.com',
-  company: '24/7 Sports',
-  tier1IABcategory: 'Sports',
-  audience: null, //string
-  contacts: [{ // array of objects
-    name: null, //string
-    email: null //string
-  }],
-  SSPaffiliation: 'Google AdX',
-  alwaysOn: false, //bool
-  ratings: {
-    1: 0, //all ratings default to 0
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    average: 0
-  },
-  channels: {
-    display: true, //bool
-    desktopVideo: false,
-    videoSSP: false,
-    mobile: false,
-    mobileSSP: false
-  },
-  availability: null, //number, represents millions/month
-  programaticPartners: {
-    us: [], //array of strings
-    restOfWorld: []
-  },
-  bidFloorPrice: null, //number - dollars
-  dealId: null, //number
-  fixed: false, //bool - can only be fixed OR auction
-  auction: false, //bool
-  highViewability: false, //bool
-  demographic: null, //string
-  history: null, //?????????? - TODO: what is this?
-  fraud: null, //?????????? - TODO: what is this?
-  notes: [{ //array of objects
-    author: null,
-    title: null,
-    text: null,
-    createdAt: null //utc datestring
-  }],
-  locale: {
-    country: null,
-    state: null,
-    city: null
-  },
-  inventory: {
-    160x600: null, //bool or null
-    300x250: null,
-    728x90: null,
-    300x600: null,
-    320x50: null
-  },
-  firstPartyData: null
+  publishers: {
+    domain: '247sports.com',
+    name: '247sports',
+    suffix: '.com',
+    company: '24/7 Sports',
+    tier1IABcategory: 'Sports',
+    audience: null, //string
+    contacts: [{ // array of objects
+      name: null, //string
+      email: null //string
+    }],
+    SSPaffiliation: 'Google AdX',
+    alwaysOn: false, //bool
+    ratings: {
+      1: 0, //all ratings default to 0
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      average: 0
+    },
+    channels: {
+      display: true, //bool
+      desktopVideo: false,
+      videoSSP: false,
+      mobile: false,
+      mobileSSP: false
+    },
+    availability: null, //number, represents millions/month
+    programaticPartners: {
+      us: [], //array of strings
+      restOfWorld: []
+    },
+    bidFloorPrice: null, //number - dollars
+    dealId: null, //number
+    fixed: false, //bool - can only be fixed OR auction
+    auction: false, //bool
+    highViewability: false, //bool
+    demographic: null, //string
+    history: null, //?????????? - TODO: what is this?
+    fraud: null, //?????????? - TODO: what is this?
+    notes: [{ //array of objects
+      author: null,
+      title: null,
+      text: null,
+      createdAt: null //utc datestring
+    }],
+    locale: {
+      country: null,
+      state: null,
+      city: null
+    },
+    inventory: {
+      160x600: null, //bool or null
+      300x250: null,
+      728x90: null,
+      300x600: null,
+      320x50: null
+    },
+    firstPartyData: null
+  }
 }
---
+++ docs/sample_json.js
@@ -0,0 +1,63 @@
+{
+  "publishers": {
+    "domain": "247sports.com",
+    "name": "247sports",
+    "suffix": ".com",
+    "company": "24/7 Sports",
+    "tier1IABcategory": "Sports",
+    "audience": "",
+    "contacts": [{
+      "name": "",
+      "email": ""
+    }],
+    "SSPaffiliation": "Google AdX",
+    "alwaysOn": false,
+    "ratings": {
+      "1": 0,
+      "2": 0,
+      "3": 0,
+      "4": 0,
+      "5": 0,
+      "average": 0
+    },
+    "channels": {
+      "display": true,
+      "desktopVideo": false,
+      "videoSSP": false,
+      "mobile": false,
+      "mobileSSP": false
+    },
+    "availability": 0,
+    "programaticPartners": {
+      "us": [""],
+      "restOfWorld": [""]
+    },
+    "bidFloorPrice": 0,
+    "dealId": "",
+    "fixed": false,
+    "auction": false,
+    "highViewability": false,
+    "demographic": "",
+    "history": "",
+    "fraud": "",
+    "notes": [{
+      "author": "",
+      "title": "",
+      "text": "",
+      "createdAt": ""
+    }],
+    "locale": {
+      "country": "",
+      "state": "",
+      "city": ""
+    },
+    "inventory": {
+      "160x600": "",
+      "300x250": "",
+      "728x90": "",
+      "300x600": "",
+      "320x50": ""
+    },
+    "firstPartyData": ""
+  }
+}
