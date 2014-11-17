d08ee4c30522f154d2cf7785db8564aacaa3f42d
Nick Escallon <nickescallon@gmail.com>
2014-11-17 02:42:47 -0500

    adds sample queries - top 10

---
+++ data/sample_queries.js
@@ -0,0 +1,20 @@
+var Firebase = require('firebase');
+var firebaseUrl = 'https://vivid-heat-6256.firebaseio.com/';
+
+//Get top 10 ratings
+function topTen() {
+  var publishers = [];
+  console.log('getting top 10 highest rated publishers');
+  var ratings = new Firebase(firebaseUrl + '/ratings');
+
+  ratings.limitToFirst(10).endAt().on('child_added', function(rating) {
+    var publisher = new Firebase( firebaseUrl + '/publishers/' );
+
+    publisher.child( rating.key() ).once('value', function(publisher) {
+      console.log( publisher.val() )
+      publishers.push( publisher.val() );
+    });
+  });
+};
+
+topTen();
