d761a0f8ac8b70c531dc6cbc7cc898c6f764a865
Nick Escallon <nickescallon@gmail.com>
2014-11-18 01:49:15 -0500

    adds ratings service, and ng repeat animations

--- public/js/home/home.controller.js
+++ public/js/home/home.controller.js
@@ -1,17 +1,23 @@
 (function() {
   'use strict'
 
-  angular.module('rottenPublishers.controllers.home', ['rottenPublishers.services.publishers'])
+  angular.module('rottenPublishers.controllers.home', [
+    'rottenPublishers.services.publishers',
+    'rottenPublishers.services.ratings'
+  ])
   .controller('homeController', homeController);
 
-  homeController.$inject = ['publishers'];
-  function homeController(publishers) {
+  homeController.$inject = ['publishers', 'ratings'];
+  function homeController(publishers, ratings) {
     var vm = this; //view model
 
     vm.publishers = publishers.data;
+    vm.ratings = ratings.data;
     vm.select = select;
 
     function select($event, index) {
+      //TODO: this isnt working well...
+      $event.preventDefault();
       $event.stopPropagation();
       vm.selected = index;
     };
---
+++ public/js/shared/services/ratings.services.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict'
+
+  angular.module('rottenPublishers.services.ratings', ['firebase'])
+  .factory('ratings', ratings);
+
+  ratings.$inject = ['$firebase'];
+  function ratings($firebase) {
+    var ratings = new Firebase('https://vivid-heat-6256.firebaseio.com/ratings');
+    var sync = $firebase(ratings);
+
+    var service = {
+      data: sync.$asObject()
+    };
+
+    return service;
+  };
+
+})();
+
