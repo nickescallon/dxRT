97ca270df8b08d3ba034c010631ad7a74f4f38c9
Nick Escallon <nickescallon@gmail.com>
2014-11-19 02:55:38 -0500

    adds routing and home template

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,29 @@
   angular.module('rottenPublishers', [
     //modules
     'ngAnimate',
+    'ngRoute',
 
     //controllers
     'rottenPublishers.controllers.home'
   ])
+  .config(config)
+
+  config.$inject = config.$inject = ['$locationProvider', '$routeProvider'];
+  function config($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);
+
+    //Routes
+    $routeProvider
+      .when('/', {
+        templateUrl: '/templates/home.template.html',
+        controller: 'homeController',
+        controllerAs: 'home'
+      })
+      .otherwise({
+        template: '<div>Otherwise!</div>'
+      });
+  }
+
 })();
+
+
